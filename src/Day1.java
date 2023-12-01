69fc0c7122c5c2bf8e25c84a2add0f8ed8412f9c
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2023-12-01 12:33:39 +0100

    Day 1 Part 2

--- src/Day1.java
+++ src/Day1.java
@@ -9,6 +9,60 @@ public class Day1 {
         try (BufferedReader reader = new BufferedReader(new FileReader("Day1-input.txt"))) {
             AtomicInteger sum = new AtomicInteger();
             reader.lines().forEach(line -> {
+                System.out.print(line + " -> ");
+                boolean changed;
+                do {
+                    changed = false;
+                    for (int i = 0; i < line.length(); i++) {
+                        if (line.startsWith("one", i)) {
+                            line = line.replace("one", "o1e");
+                            changed = true;
+                            break;
+                        }
+                        if (line.startsWith("two", i)) {
+                            line = line.replace("two", "t2o");
+                            changed = true;
+                            break;
+                        }
+                        if (line.startsWith("three", i)) {
+                            line = line.replace("three", "t3e");
+                            changed = true;
+                            break;
+                        }
+                        if (line.startsWith("four", i)) {
+                            line = line.replace("four", "f4r");
+                            changed = true;
+                            break;
+                        }
+                        if (line.startsWith("five", i)) {
+                            line = line.replace("five", "f5e");
+                            changed = true;
+                            break;
+                        }
+                        if (line.startsWith("six", i)) {
+                            line = line.replace("six", "s6x");
+                            changed = true;
+                            break;
+                        }
+                        if (line.startsWith("seven", i)) {
+                            line = line.replace("seven", "s7n");
+                            changed = true;
+                            break;
+                        }
+                        if (line.startsWith("eight", i)) {
+                            line = line.replace("eight", "e8t");
+                            changed = true;
+                            break;
+                        }
+                        if (line.startsWith("nine", i)) {
+                            line = line.replace("nine", "n9e");
+                            changed = true;
+                            break;
+                        }
+
+                    }
+                } while (changed);
+                System.out.println(line);
                 int firstDigit = line.chars().filter(c -> c >= '0' && c <= '9').findFirst().getAsInt() - '0';
                 int lastDigit = line.chars().filter(c -> c >= '0' && c <= '9').reduce((first, second) -> second).getAsInt() - '0';
                 sum.addAndGet(firstDigit * 10 + lastDigit);
