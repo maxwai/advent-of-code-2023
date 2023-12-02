17335c034d63ddaad55e7e4506fa245e8fe0d703
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2023-12-02 13:27:32 +0100

    removed import

--- src/Day2.java
+++ src/Day2.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class Day2 {
