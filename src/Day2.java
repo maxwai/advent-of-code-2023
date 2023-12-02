a1c8b4f2b12f44ccea3d2f0e635542ee6a65b6c1
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2023-12-02 13:26:45 +0100

    cleaned up to streams

--- src/Day2.java
+++ src/Day2.java
@@ -1,94 +1,79 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class Day2 {
 
     public static void main(String[] args) throws IOException {
+        Map<Integer, List<Map<String, Integer>>> input;
         try (BufferedReader reader = new BufferedReader(new FileReader("Day2-input.txt"))) {
-            System.out.println(part1(reader.lines()));
+            input = parseInput(reader.lines());
         }
-        try (BufferedReader reader = new BufferedReader(new FileReader("Day2-input.txt"))) {
-            System.out.println(part2(reader.lines()));
-        }
+        System.out.println(part1(input));
+        System.out.println(part2(input));
     }
 
-    public static int part1(Stream<String> lines) {
-        final int redCubes = 12;
-        final int greenCubes = 13;
-        final int blueCubes = 14;
-        AtomicInteger sum = new AtomicInteger();
+    public static Map<Integer, List<Map<String, Integer>>> parseInput(Stream<String> lines) {
+        Map<Integer, List<Map<String, Integer>>> gameMap = new HashMap<>();
         lines.forEach(line -> {
             String[] split = line.split(":");
-            int gameId = Integer.parseInt(split[0].split(" ")[1].trim());
-            String[] balls = split[1].trim().split("[;,]");
-            boolean possible = true;
-            ballLoop:
-            for (String ball : balls) {
-                ball = ball.trim();
-                String[] type = ball.split(" ");
-                switch (type[1].trim()) {
-                    case "red" -> {
-                        if (Integer.parseInt(type[0].trim()) > redCubes) {
-                            possible = false;
-                            break ballLoop;
-                        }
-                    }
-                    case "green" -> {
-                        if (Integer.parseInt(type[0].trim()) > greenCubes) {
-                            possible = false;
-                            break ballLoop;
-                        }
-                    }
-                    case "blue" -> {
-                        if (Integer.parseInt(type[0].trim()) > blueCubes) {
-                            possible = false;
-                            break ballLoop;
-                        }
-                    }
-                    default -> System.out.println("unknown color: " + ball);
-                }
-            }
-            if (possible)
-                sum.addAndGet(gameId);
-        });
-        return sum.get();
-    }
-
-
-    public static int part2(Stream<String> lines) {
-        AtomicInteger sum = new AtomicInteger();
-        lines.forEach(line -> {
-            String[] split = line.split(":");
-            String[] games = split[1].trim().split(";");
-            int red = 0, blue = 0, green = 0;
-            for (String game : games) {
-                String[] balls = game.split(",");
+            Integer gameId = Integer.parseInt(split[0].split(" ")[1].trim());
+            String[] pulls = split[1].trim().split(";");
+            List<Map<String, Integer>> pullList = new ArrayList<>();
+            for (String pull : pulls) {
+                String[] balls = pull.split(",");
+                Map<String, Integer> ballMap = new HashMap<>();
                 for (String ball : balls) {
                     ball = ball.trim();
                     String[] type = ball.split(" ");
-                    int ballCount = Integer.parseInt(type[0].trim());
-                    switch (type[1].trim()) {
-                        case "red" -> {
-                            if (ballCount > red)
-                                red = ballCount;
-                        }
-                        case "green" -> {
-                            if (ballCount > green)
-                                green = ballCount;
-                        }
-                        case "blue" -> {
-                            if (ballCount > blue)
-                                blue = ballCount;
-                        }
-                        default -> System.out.println("unknown color: " + ball);
-                    }
+                    ballMap.put(type[1].trim(), Integer.parseInt(type[0].trim()));
                 }
+                pullList.add(ballMap);
             }
-            sum.addAndGet(red *blue * green);
+            gameMap.put(gameId, pullList);
         });
-        return sum.get();
+        return gameMap;
+    }
+
+    public static int part1(Map<Integer, List<Map<String, Integer>>> games) {
+        Map<String, Integer> maxBalls = Map.of(
+                "red", 12,
+                "green", 13,
+                "blue", 14);
+
+        return games.entrySet().
+                stream()
+                .mapToInt(gameEntry -> gameEntry.getValue()
+                        .stream()
+                        .map(balls -> balls.entrySet()
+                                .stream()
+                                .map(ballPull -> ballPull.getValue() <= maxBalls.get(ballPull.getKey()))
+                                .reduce(true, Boolean::logicalAnd))
+                        .reduce(true, Boolean::logicalAnd) ? gameEntry.getKey() : 0)
+                .sum();
+    }
+
+
+    public static int part2(Map<Integer, List<Map<String, Integer>>> games) {
+        return games.values()
+                .stream()
+                .mapToInt(pulls -> pulls.stream()
+                        .map(pull -> List.of(
+                                pull.getOrDefault("red", 0),
+                                pull.getOrDefault("green", 0),
+                                pull.getOrDefault("blue", 0)))
+                        .reduce(List.of(-1, -1, -1), (list1, list2) -> List.of(
+                                Math.max(list1.get(0), list2.get(0)),
+                                Math.max(list1.get(1), list2.get(1)),
+                                Math.max(list1.get(2), list2.get(2))))
+                        .stream()
+                        .reduce(1, Math::multiplyExact))
+                .sum();
     }
 }
