8b6b7839709d2c121a80fc7cffb5becd8dd17294
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2023-12-02 12:58:40 +0100

    Day 2 Part 2

--- src/Day2.java
+++ src/Day2.java
@@ -59,7 +59,36 @@ public class Day2 {
 
 
     public static int part2(Stream<String> lines) {
-
-        return -1;
+        AtomicInteger sum = new AtomicInteger();
+        lines.forEach(line -> {
+            String[] split = line.split(":");
+            String[] games = split[1].trim().split(";");
+            int red = 0, blue = 0, green = 0;
+            for (String game : games) {
+                String[] balls = game.split(",");
+                for (String ball : balls) {
+                    ball = ball.trim();
+                    String[] type = ball.split(" ");
+                    int ballCount = Integer.parseInt(type[0].trim());
+                    switch (type[1].trim()) {
+                        case "red" -> {
+                            if (ballCount > red)
+                                red = ballCount;
+                        }
+                        case "green" -> {
+                            if (ballCount > green)
+                                green = ballCount;
+                        }
+                        case "blue" -> {
+                            if (ballCount > blue)
+                                blue = ballCount;
+                        }
+                        default -> System.out.println("unknown color: " + ball);
+                    }
+                }
+            }
+            sum.addAndGet(red *blue * green);
+        });
+        return sum.get();
     }
 }
