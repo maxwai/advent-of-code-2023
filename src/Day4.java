a606f2cb089b1e2c1863671fdd7b4c7ef22a17c6
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2023-12-04 07:52:06 +0100

    Day 4 Part 2

--- src/Day4.java
+++ src/Day4.java
@@ -3,7 +3,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day4 {
@@ -55,6 +54,22 @@ public class Day4 {
 
 
     public static int part2(List<List<List<Integer>>> cards) {
-        return 0;
+        Map<Integer, Integer> cardAmount = new HashMap<>();
+        for (int i = 0; i < cards.size(); i++) {
+            cardAmount.put(i, 1);
+        }
+        for (int i = 0; i < cards.size(); i++) {
+            List<Integer> winningNumbers = cards.get(i).get(1)
+                    .stream()
+                    .filter(cards.get(i).get(0)::contains)
+                    .toList();
+            for (int j = i + 1; j < i + 1 + winningNumbers.size() && j < cards.size(); j++) {
+                cardAmount.put(j, cardAmount.get(j) + cardAmount.get(i));
+            }
+        }
+        return cardAmount.values()
+                .stream()
+                .mapToInt(a -> a)
+                .sum();
     }
 }
